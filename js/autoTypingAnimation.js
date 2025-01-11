76ec300eeadb82a9d293d705519bddeabb2c4b0d
Renren <johnlawrencemartinez05@gmail.com>
2025-01-11 23:42:11 +0800

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am John Lawrence V. Martinez</span> I am a Information Technology Student and i'am 20 years old with a deep passion for information technology. I am fascinated by how technology is changing our lives and committed to being part of that change. I have expertise in electronics and programming, as well as experience as a Network Technician. In addition, I am also an active frontend and web developer, mastering HTML, CSS, and JavaScript and more, as well as frameworks like React. I enjoy creating responsive and engaging websites, combining functionality with aesthetic design. In addition, I have an interest in graphic design, where I express my creativity. I believe that technology and art can synergize to create amazing experiences. I look forward to contributing to impactful projects and continuing to learn in this field.! <br><br>  I miss you.<span>" 
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
